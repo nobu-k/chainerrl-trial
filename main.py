c0bd9686b1b91a23775562924b131b92ac876955
nobu-k <nobu.k.jp+github@gmail.com>
2018-10-12 04:24:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainerrl
+import gym
+import numpy as np
+from gym import wrappers
+
+env = gym.make('CartPole-v0')
+
+class QFunction(chainer.Chain):
+    def __init__(self, obs_size, n_actions, n_hidden_channels=50):
+        super().__init__()
+        with self.init_scope():
+            self.l0 = L.Linear(obs_size, n_hidden_channels)
+            self.l1 = L.Linear(n_hidden_channels, n_hidden_channels)
+            self.l2 = L.Linear(n_hidden_channels, n_actions)
+
+    def __call__(self, x, test=False):
+        h = F.tanh(self.l0(x))
+        h = F.tanh(self.l1(h))
+        return chainerrl.action_value.DiscreteActionValue(self.l2(h))
+
+obs_size = env.observation_space.shape[0]
+n_actions = env.action_space.n
+q_func = QFunction(obs_size, n_actions)
+
+optimizer = chainer.optimizers.Adam(eps=1e-2)
+optimizer.setup(q_func)
+
+gamma = 0.95
+explorer = chainerrl.explorers.ConstantEpsilonGreedy(epsilon=0.3, random_action_func=env.action_space.sample)
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 6)
+phi = lambda x: x.astype(np.float32, copy=False)
+
+agent = chainerrl.agents.DoubleDQN(
+    q_func, optimizer, replay_buffer, gamma, explorer,
+    replay_start_size=500, update_interval=1,
+    target_update_interval=100, phi=phi)
+
+n_episodes = 200
+max_episode_len = 200
+for i in range(1, n_episodes + 1):
+    obs = env.reset()
+    reward = 0
+    done = False
+    R = 0
+    t = 0
+    while not done and t < max_episode_len:
+        action = agent.act_and_train(obs, reward)
+        obs, reward, done, _ = env.step(action)
+        R += reward
+        t += 1
+    if i % 10 == 0:
+        print('episode:', i, 'R:', R, 'statistics:', agent.get_statistics())
+    agent.stop_episode_and_train(obs, reward, done)
+print('trained')
+
+env = wrappers.Monitor(env, "./tmp", video_callable=lambda x: True, force=True)
+obs = env.reset()
+done = False
+R = 0
+t = 0
+while not done and t < 200:
+    env.render()
+    action = agent.act(obs)
+    obs, r, done, _ = env.step(action)
+    R += r
+    t += 1
+agent.stop_episode()
+env.reset()
+env.close()
+print('recorded')
