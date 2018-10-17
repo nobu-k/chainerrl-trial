a3bc643d8401a6ca459c5d9561109b8ed7ba98b8
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2018-10-17 14:02:51 -0700

    Change python to python3

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 apt-get update
 apt-get install -y xvfb libav-tools
 echo "starting ChainerRL..."
-xvfb-run -s "-screen 0 1400x900x24" python main.py
+xvfb-run -s "-screen 0 1400x900x24" python3 main.py
